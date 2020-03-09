65dd4c3daab78299af95a71f4364babd4e13dfe0
Anatol Hleim <anatolhleim@gmail.com>
2020-03-09 23:06:35 +0300

    add Aloha

---
+++ src/test/java/action/Aloha.java
@@ -0,0 +1,12 @@
+package action;
+
+public class Aloha {
+    public static void main(String[] args) {
+        System.out.println("done");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
