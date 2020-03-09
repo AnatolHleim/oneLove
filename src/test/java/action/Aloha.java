9944fbb078dbae1e89b5a8a412da453dd235cb08
Anatol Hleim <anatolhleim@gmail.com>
2020-03-09 23:39:52 +0300

    time

--- src/test/java/action/Aloha.java
+++ src/test/java/action/Aloha.java
@@ -5,15 +5,32 @@ import org.junit.Test;
 public class Aloha {
     public static void main(String[] args) {
         System.out.println("done");
-    }
 
-    @Override
-    public String toString() {
-        return super.toString();
+        System.nanoTime();
     }
 
     @Test
     public void name() {
         
     }
+
+    public Aloha() {
+    }
+
+    @Override
+    public String toString() {
+        return "Aloha{}";
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+
 }
