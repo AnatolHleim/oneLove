8edf912bffa62a8029fa06f474b386032b0a77f8
Anatol Hleim <anatolhleim@gmail.com>
2020-03-09 21:23:54 +0300

    squash! add feature step2

---
+++ src/test/java/action/MyStep.java
@@ -0,0 +1,10 @@
+package action;
+
+import io.cucumber.java.en.Given;
+
+public class MyStep {
+    @Given("one")
+    public void one() {
+
+    }
+}
---
+++ src/test/java/action/Runner.java
@@ -0,0 +1,12 @@
+package action;
+
+import io.cucumber.junit.Cucumber;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="resources/features", glue="")
+public class Runner {
+}
